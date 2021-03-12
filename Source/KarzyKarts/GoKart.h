1ee9dcf40d345d3b45631f12da72b59b50376e9b
mertoyman <mertoyman@gmail.com>
2021-03-12 15:54:52 +0300

    Created a Go-Kart Pawn and implemented forward movement with acceleration

---
+++ Source/KarzyKarts/GoKart.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GoKart.generated.h"
+
+UCLASS()
+class KARZYKARTS_API AGoKart : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AGoKart();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+		float Mass = 1000;
+
+	//The force applied to the car when the throttle is fully down(N)
+	UPROPERTY(EditAnywhere)
+		float MaxDrivingForce = 10000;
+
+	FVector Velocity;
+
+	float Throttle;
+
+	void MoveForward(float Value);
+	
+};
